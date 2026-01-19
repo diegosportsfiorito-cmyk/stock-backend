953762e9a54f11b67110c58eb894401a118a9424
diegosportsfiorito-cmyk <diego.sports.fiorito@gmail.com>
2026-01-19 14:44:33 -0300

    Update model in ask_ai function to llama3-8b-8192

--- ai_engine.py
+++ ai_engine.py
@@ -5,7 +5,7 @@ client = Groq(api_key=os.getenv("GROQ_API_KEY"))
 
 def ask_ai(prompt):
     response = client.chat.completions.create(
-        model="llama3-70b-4096",
+        modelo="llama3-8b-8192",
         messages=[
             {
                 "role": "system",
@@ -24,3 +24,4 @@ def ask_ai(prompt):
 
 
     return response.choices[0].message["content"]
+
